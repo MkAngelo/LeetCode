48211020f47565cf6f1e437cb7e39502c1c6af53
MkAngelo <mikequi000@gmail.com>
2022-05-26 11:44:06 -0500

    Java template

---
+++ Template.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Template {
+	static final StdIn in = new StdIn();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args){
+		int t = in.nextInt();
+		for(int i=1; i<=t; ++i){
+			out.print("Case #" +i+": ");
+			new Solver();
+		}
+		out.close();
+	}
+
+	static class Solver {
+		Solver() {
+		}
+	}
+}
